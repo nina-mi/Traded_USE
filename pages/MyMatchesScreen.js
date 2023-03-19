26b8a908d97e0c5eaa8c3ca48041dfbe0f608f62
Michael-313 <126700862+Michael-313@users.noreply.github.com>
2023-03-19 23:01:10 +0100

    Update MyMatchesScreen.js
    Added a button that generates a random 4 digit code onpress

--- pages/MyMatchesScreen.js
+++ pages/MyMatchesScreen.js
@@ -1,8 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, View, Text} from "react-native";
+import { StyleSheet, View, Text, Pressable, Alert} from "react-native";
 import Button from '../components/Button';
 import { getAuth } from "firebase/auth";
+import React from "react";
 
 // style
 import { styles } from '../DefinedStyles';
@@ -12,9 +13,26 @@ export default function MyMatchesScreen({ navigation }) {
   user = auth.currentUser;
   // if (user) {
   // }
+  const [number, setNumber] = React.useState(1);
+
+  const getRandomNumber = () => {
+      const randomNumber = Math.floor(Math.random() * 9000 + 1000);
+      setNumber(randomNumber);
+  }
+
+const simpleAlertHandler = () => {
+  //function to make simple alert
+  alert(number);
+};
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>My matches</Text>
+        <Text>Press the button below to generate your locker access code</Text>
+        <Pressable 
+              style = {styles.PrimaryButton} 
+              onPress={() => { getRandomNumber(); simpleAlertHandler();}}>
+              <Text style = {styles.ButtonText}>Generate</Text>
+            </Pressable>
     </View>
+
   );
 }
